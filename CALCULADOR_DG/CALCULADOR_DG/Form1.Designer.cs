9ec273cb84235b45e68c4bd6ce2c7a1b381e015b
Eli0908 <90581387+Eli0908@users.noreply.github.com>
2023-05-16 22:24:39 -0500

    Add files via upload

---
+++ CALCULADOR_DG/CALCULADOR_DG/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace CALCULADOR_DG
+{
+    partial class CALCULADORA
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudOp1 = new System.Windows.Forms.NumericUpDown();
+            this.nudOp2 = new System.Windows.Forms.NumericUpDown();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudOp1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudOp2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(33, 161);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(75, 23);
+            this.btnSuma.TabIndex = 0;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.Location = new System.Drawing.Point(165, 161);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(75, 23);
+            this.btnResta.TabIndex = 1;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click_1);
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.Location = new System.Drawing.Point(301, 161);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicacion.TabIndex = 2;
+            this.btnMultiplicacion.Text = "*";
+            this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click_1);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Location = new System.Drawing.Point(433, 161);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(75, 23);
+            this.btnDivision.TabIndex = 3;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "OPERADOR 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "OPERADOR 2";
+            // 
+            // nudOp1
+            // 
+            this.nudOp1.Location = new System.Drawing.Point(35, 66);
+            this.nudOp1.Name = "nudOp1";
+            this.nudOp1.Size = new System.Drawing.Size(120, 20);
+            this.nudOp1.TabIndex = 6;
+            // 
+            // nudOp2
+            // 
+            this.nudOp2.Location = new System.Drawing.Point(341, 66);
+            this.nudOp2.Name = "nudOp2";
+            this.nudOp2.Size = new System.Drawing.Size(120, 20);
+            this.nudOp2.TabIndex = 7;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(33, 244);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(475, 20);
+            this.txtResultado.TabIndex = 8;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // CALCULADORA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 309);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.nudOp2);
+            this.Controls.Add(this.nudOp1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiplicacion);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSuma);
+            this.Name = "CALCULADORA";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudOp1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudOp2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnMultiplicacion;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudOp1;
+        private System.Windows.Forms.NumericUpDown nudOp2;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ CALCULADOR_DG/CALCULADOR_DG/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CALCULADOR_DG
+{
+	public partial class CALCULADORA : Form
+	{
+		public CALCULADORA()
+		{
+			InitializeComponent();
+		}
+		private void btnSuma_Click(object sender, EventArgs e)
+		{
+			 decimal v1 = this.nudOp1.Value;
+			 decimal v2 = this.nudOp2.Value;
+		   
+			 decimal total = v1 + v2;
+			  this.txtResultado.Text = total + "";
+
+		}
+
+        private void btnResta_Click_1(object sender, EventArgs e)
+        {
+            decimal v1 = this.nudOp1.Value;
+            decimal v2 = this.nudOp2.Value;
+
+            decimal total = v1 - v2;
+            this.txtResultado.Text = total + "";
+        }
+
+        private void btnMultiplicacion_Click_1(object sender, EventArgs e)
+        {
+            decimal v1 = this.nudOp1.Value;
+            decimal v2 = this.nudOp2.Value;
+
+            decimal total = v1 * v2;
+            this.txtResultado.Text = total + "";
+        }
+
+        private void btnDivision_Click_1(object sender, EventArgs e)
+        {
+            decimal v1 = this.nudOp1.Value;
+            decimal v2 = this.nudOp2.Value;
+
+            if (v2 == 0)
+            {
+                MessageBox.Show("No puede ser cero");
+            }
+
+            else
+            {
+
+                decimal total = v1 / v2;
+                this.txtResultado.Text = total + "";
+            }
+        }
+    }
+}
